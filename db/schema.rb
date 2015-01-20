7e1f8902e6c122acd0a31b94aaee5c54f771df4f
Kevin Musiorski <musiorski@gmail.com>
2015-01-20 16:22:41 +0000

    added teacher_id column to courses table, and I setup the relationship between courses and teachers

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120154303) do
+ActiveRecord::Schema.define(version: 20150120160924) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "teacher_id"
   end
 
   create_table "teachers", force: true do |t|
